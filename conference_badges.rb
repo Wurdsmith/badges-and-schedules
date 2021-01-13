868cc67c884da7e48a4c071f5fafe93d2b1682ea
Wurdsmith <wurdsmithing@gmail.com>
2021-01-13 15:43:05 -0600

    commit tree c0a46f2642aea007d71f62ccbf0dc2a224c6c0cf

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+    badge_greeting = []
+    attendees.each {|names| badge_greeting << badge_maker(names)}
+    badge_greeting
+end
+
+def assign_rooms(attendees)
+    room_list = []
+    attendees.each_with_index do |name,index|
+        room_list << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+    room_list
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each {|names| puts names}
+    assign_rooms(attendees).each {|assignment| puts assignment}
+end
